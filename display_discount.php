c5b8590ffc1f4a340043d105280de842f4dd723a
Justin Mangan <jm0015052@otc.edu>
2018-02-04 20:18:21 -0600

    Updating functionality

--- display_discount.php
+++ display_discount.php
@@ -1,19 +1,34 @@
+
 <?php
+    //  get the passed array
+    $coupons = [];
+    if (!empty($_POST['discount_array'])) {
+        $coupons = json_decode($_POST['discount_array'], true);
+    }
+
+    // prints array for troubleshooting
+    // print_r($coupons);
+
     // get the data from the form
     $product_description = filter_input(INPUT_POST, 'product_description');
     $list_price = filter_input(INPUT_POST, 'list_price');
     $discount_percent = filter_input(INPUT_POST, 'discount_percent');
-
+   
+    $quantity = filter_input(INPUT_POST, 'quantity');
+     
     // calculate the discount and discount price
-    $discount = $list_price * ($discount_percent / 100);
-    $discount_price = $list_price - $discount;
-
+    $subtotal = $list_price * $quantity;
+    $discount = $subtotal * ($discount_percent / 100);
+    $discount_price = $subtotal - $discount;
+    
     //apply currency formatting to dollar and percent ammounts
     $list_price_f = "$".number_format($list_price, 2);
+    $subtotal_f = "$".number_format($subtotal, 2);
     $discount_percent_f = $discount_percent."%";
     $discount_f = "$".number_format($discount, 2);
     $discount_price_f = "$".number_format($discount_price, 2);
 ?>
+
 <!doctype html>
 <html lang="en">
     <head>
@@ -23,11 +38,11 @@
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" href="myStyle.css">
+        <link rel="stylesheet" href="styles.css">
         <title>Product Discount Calculator</title>
     </head>
     <body>
-        <div class="container" style="height: 100vh;"  style="background: ivory;">
+        <div class="container" style="height: 100vh;" style="background: ivory;">
             <div class="row align-items-center" style="height: 100%;">
                 <div class="col-sm"></div>
                 <div class="col-sm-6">
@@ -37,14 +52,20 @@
                         </div>
                         <div class="card-body">
                             <ul class="row list-unstyled">
-                                <li class="col-6 font-weight-bold">Description:</li>
-                                <li class="col-6"><?= htmlspecialchars($product_description); ?></li>
+                                <li class="col-3 font-weight-bold">Description:</li>
+                                <li class="col-9"><?= htmlspecialchars($product_description); ?></li>
 
                                 <li class="col-6 font-weight-bold">List Price:</li>
                                 <li class="col-6"><?= htmlspecialchars($list_price_f); ?></li>
 
-                                <li class="col-6 font-weight-bold">Discount:</li>
-                                <li class="col-6"><?= htmlspecialchars($discount_percent_f); ?></li>
+                                <li class="col-6 font-weight-bold">Quantity:</li>
+                                <li class="col-6"><?= htmlspecialchars($quantity); ?></li>
+
+                                <li class="col-6 font-weight-bold">Subtotal:</li>
+                                <li class="col-6"><?= htmlspecialchars($subtotal_f); ?></li>
+
+                                <li class="col-6 font-weight-bold">Discount:<span class="font-weight-normal"><?= htmlspecialchars(" ".$coupons[$discount_percent]); ?></span></li>
+                                <li class="col-6"><?= htmlspecialchars($discount_percent_f." - ".$coupons[$discount_percent]); ?></li>
 
                                 <li class="col-6 font-weight-bold">Discount Amount:</li>
                                 <li class="col-6"><?= $discount_f; ?></li>
--- index.php
+++ index.php
@@ -1,25 +1,48 @@
+
 <?php
+    // these arrays would have more logical means of access if they were two-dimensional
     $products = [
-        "screwdriver",
-        "clapton coil"  
+        "Banded Agate Cabochon - oval 30mm x 20mm " => 10,
+        "Black Star Sapphire Cabochon - round 5mm" => 29.99,
+        "Blue Jade Cabochon - rect. 30mm x 20mm" => 35.00,
+        "Chevron Amethyst Cabochon - oval 45mm x 30mm" => 20,
+        "Hubei Turquoise Cabochon - rect. 35mm x 22mm" => 25.99,
+        "Purple Star Ruby Cabochon - round 9mm" => 21.99
     ];
+
+    $coupons = [
+        0 => "None",
+        10 => "New Customer",
+        15 => "Student",
+        20 => "Instructor",
+        25 => "Senior Citizen"
+    ];
+    
+    // used to pass the coupons array to display_discount.php via _POST
+    $pass_coupons = json_encode($coupons);
 ?>
+
+<!-- copies php products array to a JavaScript global variable for discount.js access--> 
+<script> var products = <?php echo json_encode($products); ?>; </script>
+    
+<!--HTML -->
 
 <!DOCTYPE html>
 <html lang="en">
   <head>
 <!--Student dev: Justin Mangan
     Date: 1.29.2018 -->
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="myStyle.css">
+    <link rel="stylesheet" href="styles.css">
     <title>Product Discount Calculator</title>
   </head>
-        <body>
+    <body onload="setInitialValues()">
         <div class="container" style="height: 100vh;">
             <div class="row align-items-center" style="height: 100%;">
                 <div class="col-sm"></div>
@@ -32,12 +55,11 @@
                             <form action="display_discount.php" method="post">
                                 <div class="form-group">
                                     <label for="description">Product Description</label>
-                                    <!--<input type="text" class="form-control" name="product_description">-->
-                                    <select name="description" class="form-control">
-                                     <?php foreach($products as $product): ?>
-                                        <option value="<?= $product ?>"><?= $product ?></option>
-                                     <?php endforeach; ?>
-                                     </select>
+                                    <select id="name" name="product_description" class="form-control" onchange="updatePrice()">
+                                        <?php foreach($products as $name => $price): ?>
+                                        <option value="<?= $name ?>"><?= $name ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
                                 </div>
         
                                 <div class="form-group">
@@ -46,18 +68,32 @@
                                         <div class="input-group-prepend">
                                             <span class="input-group-text">$</span>
                                         </div>
-                                        <input type="text" class="form-control" name="list_price">
+                                        <input id="price" type="text" class="form-control" name="list_price" value="" readonly>
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">each</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="quantity">Quantity</label>
+                                    <div class="input-group">
+                                        <input id="qty" type="text" class="form-control" name="quantity">
                                     </div>
                                 </div>
         
                                 <div class="form-group">
                                     <label for="discount">Discount</label>
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" name="discount_percent">
-                                        <div class="input-group-append">
-                                            <span class="input-group-text">%</span>
-                                        </div>
-                                    </div>
+                                    <select name="discount_percent" class="form-control">
+                                        <?php foreach($coupons as $percent => $label): ?>
+                                            <option value="<?= $percent ?>"><?= $percent ?>% <?= $label ?></option>
+                                        <?php endforeach; ?>
+                                    </select>
+
+                                    <!-- pass array to display_discount.php
+                                    NOTE THE USE OF SINGLE QUOTES FOR VALUE -->
+                                    <input type='hidden' name='discount_array' value='<?= $pass_coupons ?>'/>
+                                    
                                 </div>
                                 <div class="text-center">
                                     <button class="btn btn-info text-center">Calculate</button>
@@ -69,7 +105,9 @@
                 <div class="col-sm"></div>
             </div>
         </div>
-
+         
+        <!-- external JavaScript for this app -->
+        <script src="discount.js"></script>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
